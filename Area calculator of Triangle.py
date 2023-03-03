60f8c3821f6fedb35df47e8ef353724180dab3f4
Siddhartha Hingane <siddharthahingane@gmail.com>
2023-03-03 15:57:37 +0530

    Add files via upload

---
+++ Area calculator of Triangle.py
@@ -0,0 +1,7 @@
+print("This is a Area calculator of Triangle")   #intro
+print("\n")
+b=int(input("Enter the base of  the triangle:- "))   #base
+h=int(input("Enter the height of the triangle:- "))  #height
+a=0.5*b*h  #formula of area of triangle
+
+print(a)    #value
